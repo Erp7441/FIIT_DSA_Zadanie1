5b521e3215b40972a89110b92345b8c103aeb4e7
Martin Szabo <martin.szabo333@gmail.com>
2023-03-20 00:20:56 +0100

    Javadoc comments 2/2

--- src/sk/stuba/fiit/martin/szabo/hashtable/openaddressing/HashtableOpenAddressing.java
+++ src/sk/stuba/fiit/martin/szabo/hashtable/openaddressing/HashtableOpenAddressing.java
@@ -2,17 +2,56 @@ package sk.stuba.fiit.martin.szabo.hashtable.openaddressing;
 
 import sk.stuba.fiit.martin.szabo.hashtable.hashtable.Hashtable;
 
-// TODO:: Add javadoc comments
+/**
+ * {@link HashtableOpenAddressing} is a subclass of {@link Hashtable} that uses open addressing to resolve collisions.
+ * This class provides the ability to {@link #search}, {@link #insert}, and {@link #delete(Object)} key-value pairs
+ * from the hashtable. {@link HashtableOpenAddressing} overrides the {@link #search}, {@link #delete(Object)}, and
+ * {@link #resolveCollision} methods of the {@link Hashtable} class to implement open addressing.
+ * <p>
+ * {@link HashtableOpenAddressing} provides the {@link #resolveCollision} method to resolve collisions using open addressing.
+ * When a collision occurs, {@link #resolveCollision} probes the hashtable using {@link #linearProbing(int)} an empty
+ * slot is found.
+ *
+ * @see #resolveCollision
+ * @see #linearProbing(int)
+ * @see Hashtable
+ * @see #search(Object)
+ * @see #insert(Object)
+ * @see #delete(Object)
+ */
 public class HashtableOpenAddressing extends Hashtable{
 
+    /**
+     * Constructs a new empty {@link HashtableOpenAddressing} object with the default initial capacity and default
+     * load factor (0.75).
+     * @see Hashtable#Hashtable()
+     */
     public HashtableOpenAddressing(){
         super();
     }
 
+    /**
+     * Constructs a new empty {@link HashtableOpenAddressing} object with the specific capacity and default
+     * load factor (0.75).
+     * @see Hashtable#Hashtable()
+     */
     public HashtableOpenAddressing(int size){
         super(size);
     }
 
+    /**
+     * Searches for a specific value in the hash table using linear probing. If the value is found, it is returned.
+     * If the value is not found, null is returned.
+     *
+     * @param value the value to search for in the hash table
+     * @return the value if found, or null if not found
+     *
+     * @see HashtableOpenAddressing#getTable()
+     * @see HashtableOpenAddressing#getSize()
+     * @see HashtableOpenAddressing#hash(Object)
+     * @see HashtableOpenAddressing#linearProbing(int)
+     * @see HashtableOpenAddressing#get(int)
+     */
     @Override
     public Object search(Object value){
 
@@ -38,6 +77,16 @@ public class HashtableOpenAddressing extends Hashtable{
         return null;
     }
 
+    /**
+     * Searches for the specified value in the hash table using linear probing and returns its index if found.
+     * Returns -1 if the value is not found or if the value is null.
+     *
+     * @param value the value to search for in the hash table
+     * @return the index of the value if found, -1 otherwise
+     * @see #hash(Object)
+     * @see #linearProbing(int)
+     * @see #get(int)
+     */
     public int searchIndex(Object value){
 
         if(value == null){ return -1; }
@@ -60,6 +109,15 @@ public class HashtableOpenAddressing extends Hashtable{
         return -1;
     }
 
+    /**
+     * Deletes the given value from the hash table.
+     *
+     * @param value the value to be deleted from the hash table
+     * @return true if the value was deleted, false if the value was not found in the hash table
+     *
+     * @see #searchIndex(Object)
+     * @see #deleteValue(int)
+     */
     @Override
     public boolean delete(Object value){
 
@@ -79,6 +137,16 @@ public class HashtableOpenAddressing extends Hashtable{
         return true;
     }
 
+    /**
+     * Resolves a collision at a given index by linearly probing the table for the next available slot
+     * and setting the provided value at that index.
+     *
+     * @param index the index at which the collision occurred
+     * @param value the value to be stored in the table
+     *
+     * @see #linearProbing(int)
+     * @see #set(int, Object)
+     */
     @Override
     public void resolveCollision(int index, Object value){
         Object current = this.get(index);
--- src/sk/stuba/fiit/martin/szabo/hashtable/separatechaining/HashtableSeparateChaining.java
+++ src/sk/stuba/fiit/martin/szabo/hashtable/separatechaining/HashtableSeparateChaining.java
@@ -1,11 +1,29 @@
 package sk.stuba.fiit.martin.szabo.hashtable.separatechaining;
 
 import sk.stuba.fiit.martin.szabo.hashtable.hashtable.Hashtable;
+import sk.stuba.fiit.martin.szabo.hashtable.openaddressing.HashtableOpenAddressing;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
 
-// TODO:: Add javadoc comments
+/**
+ * A class that implements a hash table with separate chaining collision resolution. Each index in the hash table can
+ * contain multiple values in a {@link LinkedList}, with each value having its own hash code. When a new value is added
+ * to the hash table, the index is determined by hashing the value, and any collisions are resolved by adding the value
+ * to the {@link LinkedList} at that index. When a value is searched for or deleted, the index is determined in the same
+ * way, and the {@link LinkedList} at that index is traversed until the value is found or the end of the list is reached.
+ * <p>
+ * This class extends the {@link Hashtable} abstract class and overrides the {@link #search} and {@link #resolveCollision}
+ * methods to implement separate chaining collision resolution.
+ * <p>
+ * The {@link #resolveCollision(int, Object)} method is used to add a value to the hash table when a collision occurs.
+ * It first searches for the value in the hash table using the search method, and then adds the value to the {@link LinkedList}
+ * at that index if it is found. If the value is not found, a new {@link LinkedList} is created and added to the index.
+ *
+ * @see Hashtable
+ * @see HashtableOpenAddressing
+ * @see LinkedList
+ */
 public class HashtableSeparateChaining extends Hashtable{
 
     public HashtableSeparateChaining(){
@@ -16,6 +34,15 @@ public class HashtableSeparateChaining extends Hashtable{
         super(size);
     }
 
+    /**
+     * When searching for a value, the hash code of the value is used to find the index in the hash table where it
+     * may be located. If the value is not found at that index, the {@link LinkedList} at that index is traversed until
+     * the value is found or the end of the list is reached. If the end of the list is reached, the search terminates and
+     * returns null. If the value is found in the {@link LinkedList}, it is returned.
+     *
+     * @param value the value to search for in the Hashtable
+     * @return the found value or null if it does not exist in the Hashtable
+     */
     @Override
     public Object search(Object value){
 
@@ -76,6 +103,16 @@ public class HashtableSeparateChaining extends Hashtable{
         return null;
     }
 
+    /**
+     * Searches the given {@link LinkedList} for the given value. If the value is found, it is returned. If the value
+     * is not found, null is returned. If the {@link LinkedList} contains a deleted value (marked by the special
+     * {@link #DELETED_VALUE} object), it is skipped and the search continues. This method is used by the
+     * {@link HashtableSeparateChaining} class to search {@link LinkedList}s for values.
+     *
+     * @param list the linked list to search
+     * @param value the value to search for in the linked list
+     * @return the value if found in the linked list, null otherwise
+     */
     public static Object searchLinkedList(LinkedList<Object> list, Object value){
         for(Object item : list){
             if(item.equals(Hashtable.DELETED_VALUE)) continue;
@@ -84,6 +121,20 @@ public class HashtableSeparateChaining extends Hashtable{
         return null;
     }
 
+    /**
+     * Resolves a collision that occurs during the insertion of a value into the hash table. If the index of the
+     * hash table is already occupied with a value, the {@link #resolveCollision} method is called to
+     * {@link #insert(Object)} the value using separate chaining. The method first searches for the value in the hash
+     * table using the {@link #search(Object)} method. If the {@link #search(Object)} returns a {@link LinkedList},
+     * the value is added to the end of that list. If the search returns a single value, a new {@link LinkedList} is
+     * created containing both the existing value and the new value, and the {@link LinkedList} is added to the hash
+     * table. If the {@link #search(Object)} returns null, a new {@link LinkedList} is created containing only the
+     * new value and the {@link LinkedList} is added to the hash table.
+     *
+     * @param index the index in the hash table where the value is to be inserted
+     * @param value the value to insert into the hash table
+     * @see Hashtable#search
+     */
     public void resolveCollision(int index, Object value){
 
         Object tableValue = this.search(value);
